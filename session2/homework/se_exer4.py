2f67c3bc3643a9cc342fc6fbc16dcd6dfff3da82
DNQ1408 <hoadd.2014@gmail.com>
2017-11-26 02:56:25 +0700

    Initial commit

--- session2/homework/se_exer4.py
+++ session2/homework/se_exer4.py
@@ -1,16 +1,16 @@
-# n = int(input("A number, please:"))
-# for i in range(n):
-#     print(end="*")
-#
-# for i in range(n):
-#     print(end="X*")
+n = int(input("A number, please:"))
+for i in range(n):
+    print(end="*")
+
+for i in range(n):
+    print(end="X*")
 
 c = int(input("Number of columns:"))
 r = int(input("Number of rows:"))
-# for i in range(r):
-#     for i in range (c):
-#         print(end="*")
-#     print()
+for i in range(r):
+    for i in range (c):
+        print(end="*")
+    print()
 
 for i in range (r):
     if i % 2 == 0:
