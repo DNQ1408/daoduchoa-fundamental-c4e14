edbf74921d5a059dd3c31622fe187e77cb995670
DNQ1408 <hoadd.2014@gmail.com>
2017-12-02 00:38:43 +0700

    Commit

--- session4/binary_search.py
+++ session4/binary_search.py
@@ -2,17 +2,16 @@ nums = [-55,0,10,12,16,20,33]
 x = int(input('Enter a number:'))
 
 found = False
-found_index = -1
 lo = 0
 hi = len(nums)
-while hi > lo:
+while lo < hi and not found:
     mid = (lo + hi) // 2
     num = nums[mid]
     if x == num:
         found = True
     elif x < num:
-        hi = mid
-    elif x > num:
+        hi = mid # hi = mid - 1 với điều kiện đầu hi = len() - 1 và while lo < hi
+    else:
         lo = mid + 1
 
 if not found:
